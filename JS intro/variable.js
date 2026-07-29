d4197f4acf21ab958be7ff1aa57b950c8d288ca3
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-07-30 01:35:01 +0600

    Fix formatting and clarify variable declaration comments in variable.js

--- JS intro/variable.js
+++ JS intro/variable.js
@@ -1,6 +1,22 @@
 /* 
 VARIABLE ==> vary + able
 
-* % things to declare a variable
+* 5 things to declare a variable
 
 */
+var weight = 38;
+var price = 400;
+var year = 2000;
+var money = 250;
+
+/* This is undefined because there is no value set on age
+var age;
+console.log(age); */
+
+// Here the value defined 
+console.log(weight);
+console.log(price);
+console.log(year);
+console.log(money);
+// undefined
+// console.log(age); 

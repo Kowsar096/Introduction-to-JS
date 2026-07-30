39ef0939dfb36797dbcf010294ddc86bad580194
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-07-30 21:33:46 +0600

    Add types.js with variable declarations and type checks

---
+++ JS intro/types.js
@@ -0,0 +1,22 @@
+//number types variable
+
+var age = 24;
+console.log(age);
+
+// String types variable
+
+var name = "JavaScript";
+console.log(name);
+
+//boolean types variable
+
+var isRich = true;
+console.log(isRich);
+
+var passed = false;
+console.log(passed);
+
+// type check
+
+console.log(typeof passed);
+console.log(typeof name);

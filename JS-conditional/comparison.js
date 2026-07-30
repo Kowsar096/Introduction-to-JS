c0e7e8a388081e1199e6a7f26c9a6d8a1b44b4c7
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-07-31 01:52:30 +0600

    Add comparison examples and variable declarations in comparison.js and variable.js

---
+++ JS-conditional/comparison.js
@@ -0,0 +1,34 @@
+/** 
+ * COMPARISON
+ * 1. bigger: >
+ * 2. less: <
+ * 3. equal: ==
+ * 4. greater than or equal: >=
+ * 5. less than or equal: <=
+ * 6. not equal: !=
+ * 
+ * ----------------------
+ * 7. and: && (two condition satisfy)
+ * 8. or: || (only one condition satisfy)
+ * 
+ * */
+
+console.log(5 > 10); //false
+console.log(5 < 10); //true
+console.log(5 == 10); //false
+console.log(10 == 10); //true
+console.log(10 >= 10); //true
+console.log(10 >= 10); //true
+console.log(15 <= 10); //false
+
+//------------------------------
+
+var guyabaPrice = 20;
+var applePrice = 50;
+console.log(guyabaPrice > applePrice); //false
+console.log(guyabaPrice < applePrice); //true
+
+/**
+ * ==
+ * ===
+ *  **/
---
+++ JS-conditional/variable.js
@@ -0,0 +1,26 @@
+/** 
+ * variable ==> container
+ * 
+ * 
+ **/ 
+
+//number
+var sunglass = 80;
+
+//string
+var subject = "CSE"
+
+// boolean
+
+var passed = true;
+
+/** *
+ * NAMING CONVERSATION
+ * 1. SINGLE WORD
+ * 2. NO QUOTE
+ * 3. NO GAP OR DASH
+ * 4. CAN NOT START WITH NUMBER
+ * 5. CAN NOT BE A KEYWORD
+ * 6. PREFER CAMELCASE
+ *
+*/

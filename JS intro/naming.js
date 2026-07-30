cc97972a7b2960294babf8abee8cecdcb472a200
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-07-30 23:09:55 +0600

    Add naming conventions and examples for variable declarations in naming.js

---
+++ JS intro/naming.js
@@ -0,0 +1,39 @@
+// 1. no keyword in variable name
+
+var false = 45;
+var var = 50;
+
+// 2. no space or gap in variable name
+
+var is happy = false;
+var ishappy = false;
+
+// 3. no quote
+
+var "address" = "Dhaka";
+
+// 4. can not start with number
+
+var 1money = 50;
+
+// 5. name is case sensitive
+
+address
+ADDRESS
+address
+
+// 6. how to write a long variable name 
+
+var my current home address = "Tangail";
+var my_current_home_address = "Tangail"; //snake case
+var myCurrentHomeAddress = "Tangail"; //camel case
+var MyCurrentHomeAddress = "Tangail"; //pascal case
+
+// 7. Keyword are case sensitive
+
+Var age = 20; //it's not correct
+var age = 20; //it's correct
+
+// 8. don't add any symbol in first
+
+var -age = 20;

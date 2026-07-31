06d11ae807b1cae5b3d9df1acd6f8d8700e7f54f
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-01 02:46:45 +0600

    Add default and shortcut conditional examples in shortcut.js

---
+++ JS-conditional/shortcut.js
@@ -0,0 +1,29 @@
+const isLeader = true;
+
+//Default conditional case for true or false
+
+if (isLeader === true){
+    console.log('Stand up');
+}
+else{
+    console.log('Sit Down');
+
+}
+//Shortcut form condition for TRUE case
+if(isLeader){
+    console.log("Stand up");
+}
+else{
+    console.log('Sit Down');
+
+}
+
+//Shortcut form condition for false case
+
+if(!isLeader){
+    console.log("Stand up");
+}
+else{
+    console.log('Sit Down');
+
+}

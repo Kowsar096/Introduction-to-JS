f6aee109fc41be812cb4df2c0df880759591aa2c
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-03 23:52:20 +0600

    Add while loop example with conditional check

---
+++ JS_loops/while.js
@@ -0,0 +1,14 @@
+const number = 7;
+if (number < 10) {
+    console.log("The number is less than 10.");
+}
+console.log("This line will always execute.");
+
+
+// ----------------WHILE LOOP----------------
+
+const num = 0;
+while (num < 5){
+    console.log('Loop iteration:', num);
+    num = num + 1;
+}

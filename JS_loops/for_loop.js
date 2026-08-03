3949dc8ac4f6e4a905cd957b83c68eb693bb2941
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-03 23:33:44 +0600

    Add for loop examples for numbers and fruits

---
+++ JS_loops/for_loop.js
@@ -0,0 +1,15 @@
+const numbers = [12, 98, 34, 56, 78, 90];
+
+for (const num of numbers) {
+  console.log(num);
+}
+
+const fruits = ['apple', 'banana', 'cherry', 'date'];
+
+// for(){
+
+// }
+
+for (const fruit of fruits) {
+  console.log(fruit);
+}

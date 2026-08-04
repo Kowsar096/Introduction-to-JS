9c1904c00fb4d019539f7b9dec482162a3db7790
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-04 23:54:39 +0600

    Add example of while loop with even number check

--- JS_loops/range.js
+++ JS_loops/range.js
@@ -14,3 +14,14 @@ while (num < 10) {
 //     console.log(num2);
 //     num2++;
 // }
+
+
+let number = 1;
+while(number <= 10){
+    console.log(number);
+    if (number % 2 ===0){
+        console.log('Even number:',number);
+    }
+    number ++;
+
+}

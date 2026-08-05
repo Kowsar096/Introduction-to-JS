f161d144a27ca10b42dfe4c4728f0305f23288ca
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-05 23:31:13 +0600

    Refactor for loop examples and add sum calculation from 10 to 20

--- JS_loops/for.js
+++ JS_loops/for.js
@@ -1,6 +1,21 @@
-for (let i = 1; i <= 10; i++) {
-    console.log(i);
-    if (i % 2 === 0) {
-        console.log('Even number:', i);
-    }
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+//Even numbers from 1 to 10
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//     if (i % 2 === 0) {
+//         console.log('Even number:', i);
+//     }
+// }
+
+//Sum 10 - 20
+let sum = 0;
+for (let k=10; k<=20; k++) {
+    sum = sum + k;
+    console.log(k);
+
 }
+console.log('Sum of numbers from 10 to 20 is:', sum);
+

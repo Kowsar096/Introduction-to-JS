6e473a96ee1d8fd24bb090175affc95ad0b3bd7f
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-07 01:11:49 +0600

    Add push and pop examples for array manipulation

---
+++ JS_array/push-pop.js
@@ -0,0 +1,12 @@
+const numbers = [12, 87, 44, 56]
+
+console.log(numbers);
+
+numbers.push(69);
+numbers.push(79);
+numbers.push(89);
+console.log('After push: ', numbers);
+
+const out = numbers.pop();
+console.log('After pop: ', numbers);
+console.log('Pop number: ', out);

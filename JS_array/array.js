940ad281804d8c46054a82ab2d58a6c226285b2d
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-07 00:17:28 +0600

    Add initial array examples and demonstrate array indexing and length calculation

---
+++ JS_array/array.js
@@ -0,0 +1,4 @@
+const numbers = [5, 2, 3, 0 , 1];
+const string = ['Number', 'Loop', 'Array'];
+const passed = [true, false, true];
+const mixedArray = [12, 'Address', true, 45.56];
---
+++ JS_array/index.js
@@ -0,0 +1,13 @@
+// array index [0, 1, 2, 3 ......];
+
+const number = [15, 221, 43, 50, 2, 4, 22, 44, 55, 88];
+
+console.log(number);
+
+//get element value by index
+console.log(number[3]); // index 3 means 50 (4th)
+
+// set or update element value by index
+
+number[1] = 69; // update the value using index
+console.log(number);

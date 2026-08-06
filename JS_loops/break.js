ee8e6767c1e902499331865595bbdf827f44268d
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-06 23:34:41 +0600

    Add break and continue examples for loop and while loop

---
+++ JS_loops/break.js
@@ -0,0 +1,15 @@
+for (let i = 0; i< 25; i++){
+    console.log(i);
+    if(i >= 5){
+        break ;
+    }
+}
+console.log('Break');
+let j = 54;
+while (j > 25){
+    console.log(j);
+    if(j <= 50){
+        break ;
+    }
+    j--;
+}
---
+++ JS_loops/continue.js
@@ -0,0 +1,25 @@
+// continue --> skip rest of the code for this iteration
+// break --> i am done with this loop. loop end
+
+//For loop
+
+for (let i = 1; i < 10 ; i++){
+    if (i % 2 === 1){
+        continue;
+    }
+    console.log(i);
+}
+
+
+
+//while loop
+console.log('While Loop')
+let n = 0;
+while (n < 50){
+    n++;
+    if(n % 5 !== 0){
+        continue;
+
+    }
+    console.log(n);
+}

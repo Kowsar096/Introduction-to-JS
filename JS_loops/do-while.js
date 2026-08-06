e3540b2effac2ebd699960116b603761b5d50c7f
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-06 23:51:39 +0600

    Add initial implementation of do-while loop example

---
+++ JS_loops/do-while.js
@@ -0,0 +1,19 @@
+//almost we don't use it
+
+let i = 0;
+while(i < 5){
+    console.log(i);
+    i++;
+}
+
+
+
+//do while loop 
+console.log('Do While Loop');
+
+let n = 10;
+do{
+    console.log(n);
+    n++;
+}
+while (n < 5)  

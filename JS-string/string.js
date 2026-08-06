aba4fa1ef729f30dfd10e08fb96ae95c37a29518
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-07 02:13:06 +0600

    Add initial string and array examples with type checks and length calculations

---
+++ JS-string/string.js
@@ -0,0 +1,25 @@
+const country = 'Bangladesh';
+const division = "Dhaka";
+const district = `Tangail`;
+const thana = new String ('Khilkhet');
+
+
+console.log(typeof country); //String
+console.log(typeof division); //String
+console.log(typeof district); //String
+console.log(typeof thana); //Object
+
+
+const numbers = [12, 87, 44, 56]
+
+console.log (numbers.length); //length of an array
+console.log (numbers[1]); // element of the index number
+
+console.log (division.length); //string length
+console.log (division[1]); //element of the index in string
+
+//String is immutable : its means you cant change element
+
+
+
+

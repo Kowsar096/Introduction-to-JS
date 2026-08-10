7c3ef9b6c196598e8a200186c4914f71ef0135c6
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-11 01:23:00 +0600

    Add initial object and variable examples with console logging

---
+++ JS-string/object.js
@@ -0,0 +1,22 @@
+const age = 26;
+const school = 'Rajuk Uttara';
+const isPassed = true;
+// let isDeveloper; //undefined
+const subject = ['Bangla', 'English', 'Math', 'Physics']
+
+console.log(isDeveloper);
+
+//object
+
+const bottle = {
+    brand: 'Pran',
+    price: 45,
+    color: 'whilte',
+    isClean: false,
+    distributor: {
+        salam: '5pics',
+        kalam: '111pics'
+    }
+
+}
+

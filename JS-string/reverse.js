196a4d8ec9a79b2b5eb07e91651d122f1cee840f
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-11 00:05:44 +0600

    Add examples for reversing a string using different methods

---
+++ JS-string/reverse.js
@@ -0,0 +1,25 @@
+const sentence = 'I am learning web dev';
+
+ //reverse
+
+let reverse = '';
+
+for (const letter of sentence){
+    // console.log(letter);
+    reverse = letter + reverse;
+}
+console.log(reverse);
+
+//another way to reverse a string
+let rev = '';
+for (let i = 0; i<sentence.length; i++){
+    // console.log(i);
+    // console.log(sentence[i]);
+    const letter = sentence [i];
+    rev = letter + rev;
+}
+console.log(rev);
+
+//shortcut way
+const reversed = sentence.split('').reverse().join('');
+console.log(reversed);

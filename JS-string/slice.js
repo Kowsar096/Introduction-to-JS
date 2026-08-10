283ca3c06775187ac5ae8f4225b16e5c7db639f4
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-08-10 23:43:36 +0600

    Add examples for string manipulation using slice, split, and join methods

---
+++ JS-string/slice.js
@@ -0,0 +1,17 @@
+const address = 'Dhaka Tangail';
+const part = address.slice(2,5);
+console.log(part);
+
+const sentence = 'Jamal is a good and hardworking person';
+
+// split() divides a string into multiple parts wherever it finds the specified separator.
+console.log(sentence.split(' '));
+console.log(sentence.split('a')); 
+
+//join() combines the elements of an array into a single string, using the specified separator.
+
+const words = ["I", "love", "JavaScript"];
+const result = words.join(" ");
+
+console.log(result);
+
